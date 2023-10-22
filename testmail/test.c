3952181ca48a678e22ae3f4d1eac9106ccd1b97c
wangziao <1575538687@qq.com>
2023-10-22 13:47:25 -0700

    add test code for phase 1

---
+++ testmail/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main()
+{
+	char *filename = "../vfs/foo";
+	int my_file_desc = open(filename, O_WRONLY|O_CREAT, 0600);
+	char *p = strdup("This can the email's content");
+	int write_len = strlen(p);
+	int w = write(my_file_desc, p, write_len);
+	close(my_file_desc);
+	return 0;
+}
