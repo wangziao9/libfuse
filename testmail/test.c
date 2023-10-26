3375e23428732b9c968b16c0cecd1bc1adc61443
wangziao <1575538687@qq.com>
2023-10-25 23:12:33 -0700

    change default email content

--- testmail/test.c
+++ testmail/test.c
@@ -8,7 +8,7 @@ int main()
 {
 	char *filename = "../vfs/foo";
 	int my_file_desc = open(filename, O_WRONLY|O_CREAT, 0600);
-	char *p = strdup("This can the email's content");
+	char *p = strdup("This is an email from group iloveos.");
 	int write_len = strlen(p);
 	int w = write(my_file_desc, p, write_len);
 	close(my_file_desc);
